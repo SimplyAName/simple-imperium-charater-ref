aad4476f6ee16113fe9c8770b7fee208fc3525b9
Michael Loney <michael.loney137+git@gmail.com>
2026-08-30 22:25:11 +0100

    Lint and formatting

--- dev-scripts/refine-data/traits/null-traits.ts
+++ dev-scripts/refine-data/traits/null-traits.ts
@@ -1,12 +1,12 @@
 export function isTraitsEmpty(traitsValue: string) {
-  if (
-    traitsValue === null ||
-    traitsValue === undefined ||
-    traitsValue === "" ||
-    traitsValue === "-"
-  ) {
-    return true;
-  }
+	if (
+		traitsValue === null ||
+		traitsValue === undefined ||
+		traitsValue === "" ||
+		traitsValue === "-"
+	) {
+		return true;
+	}
 
-  return false;
+	return false;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,27 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+
 import "./index.css";
-import { routeTree } from './routeTree.gen'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { createRoot } from "react-dom/client";
+
+import { routeTree } from "./routeTree.gen";
 
 // Set up a Router instance
 const router = createRouter({
-  routeTree,
-  defaultPreload: 'intent',
-  scrollRestoration: true,
-})
+	routeTree,
+	defaultPreload: "intent",
+	scrollRestoration: true,
+});
 
 // Register things for typesafety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
 }
 
-createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
