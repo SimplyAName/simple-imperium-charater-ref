b2789b49c804ae9051804af8f10809b1ad9a8b0a
Michael Loney <michael.loney137+git@gmail.com>
2026-08-30 21:23:30 +0100

    Parse formatted numbers

--- dev-scripts/utils/parseTypesUtils.ts
+++ dev-scripts/utils/parseTypesUtils.ts
@@ -1,14 +1,16 @@
 export function parseNumberOrText(value: string | number): number {
 	if (typeof value === "number") return value;
+
+	// Remove commas for numbers formatted with them. E.g: 1,200 -> 1200
+	value = value.replaceAll(/,/g, "");
+
 	return Number.parseInt(value);
 }
 
 export function parseRequiredNumber(value: string | number, fieldName: string): number {
-	const parsedValue = typeof value === "number" ? value : Number.parseInt(value, 10);
+	const parsedValue = typeof value === "number" ? value : Number.parseInt(value);
 	if (Number.isNaN(parsedValue)) {
 		throw new Error(`Expected ${fieldName} to be numeric, received: ${value}`);
 	}
 	return parsedValue;
 }
-
-
